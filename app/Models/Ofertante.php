9078a7e89a88cc3b2724ec2e81b84865b54cbb0c
JericoCV <jericoforeveralone@gmail.com>
2021-05-21 15:15:37 -0500

    modelos y controladores

---
+++ app/Models/Ofertante.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ofertante extends Model
+{
+    use HasFactory;
+}
