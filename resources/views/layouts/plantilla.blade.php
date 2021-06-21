1ef99ff578fa730fd89324cd95b291a6b9f7ed48
JericoCV <jericoforeveralone@gmail.com>
2021-06-20 19:30:16 -0500

    maquetacion 1

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -10,5 +10,19 @@
 </head>
 <body>
 @yield('content')
+<footer>
+    <a href="https://api.whatsapp.com/send?phone=51969449142" target="_blank"><img src="{{asset('images/img.png')}}" width="30px"></a>
+    <a href="mailto: jerico.cajahuaman.valera@gmail.com" target="_blank"><img src="{{asset('images/img_1.png')}}" width="30px"></a>
+</footer>
 </body>
+<script>
+    var loadFile = function(event) {
+        var reader = new FileReader();
+        reader.onload = function(){
+            var output = document.getElementById('output');
+            output.src = reader.result;
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    };
+</script>
 </html>
